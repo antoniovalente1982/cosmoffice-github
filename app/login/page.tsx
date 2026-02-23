85dd14d6536a12cf211aff6894f87ae2ab0547e6
Antonio <valenteantonio1982@gmail.com>
2026-02-23 17:14:08 +0100

    fix: unescaped react entities in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -94,7 +94,7 @@ export default function LoginPage() {
 
           <div className="mt-6 text-center">
             <p className="text-sm text-slate-400">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <Link href="/signup" className="text-primary-400 hover:text-primary-300 font-medium">Sign up</Link>
             </p>
           </div>
